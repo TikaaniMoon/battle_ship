28d348260a8935d077a652d21e76ae26251cac49
Anton Pivnenko <pivnenkowebdev@gmail.com>
2025-02-06 20:58:21 +0400

    fix: fix arguments

--- main.py
+++ main.py
@@ -1,31 +1,30 @@
 class Field:
     def __init__(self, size, ships):
         self.size = size
-        self.ships_alive = ships
         self.grid = []
         for _ in range(size):
             self.grid.append([None] * size)
+        self.ships_alive = ships
 
+# тут корабли изначально скрыты, нужно передать True
     def display(self, show_ships=False):
-
-        # вывод строки с буквами
-        letters = 'ABCDEFGHIJ'
-        letter_space = '     '
-
+        letters = "ABCDEFGHIJ"
+        letter_string = "    "
         for letter in letters:
-            letter_space += letter + ' '
+            letter_string += letter + " "
 
+        print(letter_string)
         for i, row in enumerate(self.grid):
             display_row = ""
             for cell in row:
                 if cell is None or (cell is not None and not show_ships):
-                    display_row += 'O '
+                    display_row += "O "
                 else:
-                    display_row += '■ '
+                    display_row += "■ "
             if i + 1 != 10:
-                print(i + 1, '', display_row)
+                print(i + 1, " ", display_row)
             else:
-                print(i + 1, '', display_row)
+                print(i + 1, "", display_row)
 
-Titanik = Field(10, 1)
-Titanik.display()
+test = Field(10, 1)
+test.display();
