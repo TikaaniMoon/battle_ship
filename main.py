11f7a96e2e5d6866afacc398f91f0be947176ada
Tikaani <dara.stormy@gmail.com>
2025-02-08 15:21:22 +0200

    feat: add class BattleshipGame and function place_ships_randomly

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import random
+
 class Field:
     def __init__(self, size, ships):
         self.size = size
@@ -26,5 +28,47 @@ class Field:
             else:
                 print(i + 1, "", display_row)
 
-test = Field(10, 1)
-test.display();
+
+class BattleshipGame:
+    def __init__(self):
+        self.size = 10
+        self.ships = 15
+        self.player_field = Field(self.size, self.ships)
+        self.computer_field = Field(self.size, self.ships)
+
+    # Это функция расстановки кораблей, она уже полностью написана
+    def place_ships_randomly(self, field, num_ships):
+        for _ in range(num_ships):
+            placed = False
+            while not placed:
+                coords = (random.randint(0, self.size - 1), random.randint(0, self.size - 1))
+                if self.is_valid_ship_placement(field, coords):
+                    field.grid[coords[0]][coords[1]] = "S"
+                    placed = True
+
+    # Это функция проверки расстановки кораблей, она уже полностью написана
+    def is_valid_ship_placement(self, field, coords, ship_length=1, ):
+        x, y = coords
+
+        # Проверка на наличие соседних клеток по горизонтали и вертикали
+        for i in range(ship_length + 2):
+            for j in range(-1, 2):
+                for k in range(-1, 2):
+                    new_x, new_y = x + j, y + k
+                    if 0 <= new_x < self.size and 0 <= new_y < self.size and field.grid[new_x][new_y] == "S":
+                        return False
+
+        return True
+
+    def play(self):
+
+        print("Расстановка кораблей компьютера:")
+        self.place_ships_randomly(self.computer_field, self.ships)
+        self.computer_field.display()
+
+        print("Ваша расстановка кораблей:")
+        self.place_ships_randomly(self.player_field, self.ships)
+        self.player_field.display(show_ships=True)
+
+
+
